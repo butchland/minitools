c8b11f928327009c0b29f4fbd66e627c0c43dc4d
Butch Landingin <butchland@gmail.com>
2022-11-02 23:10:11 +0800

    feat: Update main page to show example and remove placeholders

--- minitools/_modidx.py
+++ minitools/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://butchland.github.io',
                 'git_url': 'https://github.com/butchland/minitools',
                 'lib_path': 'minitools'},
-  'syms': {'minitools.utils': {'minitools.utils.foo': ('utils.html#foo', 'minitools/utils.py')}}}
+  'syms': { 'minitools.utils': { 'minitools.utils.conv_img_size': ('utils.html#conv_img_size', 'minitools/utils.py'),
+                                 'minitools.utils.tconv_img_size': ('utils.html#tconv_img_size', 'minitools/utils.py')}}}
